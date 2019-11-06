8ee2ced7dca70ce060737dfa539855f47f6bec5d
Knar <knarxr@protonmail.com>
2019-11-06 11:30:04 -0600

    using prototype functions

--- c.c
+++ c.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int sum(int x, int y);
+
 main ()
 {
-  printf ("Hello World\n");
+  int theSum = sum(10, 11);
+  printf("Sum: %i\n", theSum);
+}
+
+int sum(int x, int y)
+{
+  return x + y;
 }
